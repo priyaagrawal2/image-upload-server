586b2b1084d2269f618c9bde09eedfa71056e835
Priya Agarwal <priyaagarwal1704@gmail.com>
2026-05-02 20:45:02 +0530

    Fixed upload page and added homepage

--- app.py
+++ app.py
@@ -1,43 +1,35 @@
-from flask import Flask, request, jsonify
-import boto3
-import uuid
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 
-s3 = boto3.client('s3')
-BUCKET = "image-upload-priya123"
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-@app.route('/upload', methods=['POST'])
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
 def upload():
-    if 'file' not in request.files:
-        return jsonify({"error": "No file"}), 400
+    if "file" not in request.files:
+        return "No file selected"
 
-    file = request.files['file']
+    file = request.files["file"]
 
-    if file.filename == '':
-        return jsonify({"error": "Empty filename"}), 400
+    if file.filename == "":
+        return "No file selected"
 
-    if not file.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-        return jsonify({"error": "Only images allowed"}), 400
+    file_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+    file.save(file_path)
 
-    file.seek(0, os.SEEK_END)
-    size = file.tell()
-    file.seek(0)
+    return "Image uploaded successfully ✅"
 
-    if size > 2 * 1024 * 1024:
-        return jsonify({"error": "Max 2MB allowed"}), 400
 
-    filename = str(uuid.uuid4()) + file.filename
-
-    s3.upload_fileobj(file, BUCKET, filename, ExtraArgs={"ContentType": file.content_type})
-
-    url = f"https://{BUCKET}.s3.amazonaws.com/{filename}"
-
-    return jsonify({"url": url})
-
-if __name__ == '__main__':
-    import sys
-    port = int(sys.argv[1])
-    print("Handled by port:", port)   
-    app.run(port=port)
+if __name__ == "__main__":
+    app.run(debug=True)
